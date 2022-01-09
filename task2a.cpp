aad9d035780051c74c1375950269bc644857960c
ShreyaSrivastava12 <91174445+ShreyaSrivastava12@users.noreply.github.com>
2022-01-09 19:12:58 +0530

    Create task2a.cpp
    Calling and Execution flow of constructor

---
+++ task2a.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class Parent{
+    public:
+    Parent()
+    {
+        cout<<" Parent class constructor\n";
+    }
+};
+class Child : public Parent{
+    public:
+    Child()
+    {
+        cout<<" Child class constructor\n";
+    }
+};
+int main()
+{
+    Child obj;
+}
+ 
